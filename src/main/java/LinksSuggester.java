1b54ebb424bb2079fecffd6a939455864722903c
Shamin1989 <shamindp@mail.ru>
2022-06-05 12:11:34 +0300

    diploma

--- src/main/java/LinksSuggester.java
+++ src/main/java/LinksSuggester.java
@@ -1,13 +1,50 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LinksSuggester {
 
-    public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
+    private final File file;
 
+    public LinksSuggester(File file) throws FileNotFoundException {
+        if (file.isFile()) {
+            this.file = file;
+        } else {
+            throw new FileNotFoundException("File not found");
+        }
     }
 
     public List<Suggest> suggest(String text) {
-        return null;
+        List<Suggest> allSuggests = getAllSuggests();
+        List<Suggest> findSuggests = new ArrayList<>();
+        for (Suggest sug : allSuggests) {
+            if (text.toLowerCase().contains(sug.getKeyWord().toLowerCase())) {
+                findSuggests.add(sug);
+            }
+        }
+        return findSuggests;
+    }
+    public List<Suggest> getAllSuggests() {
+        List<Suggest> allSuggests = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(file);
+            if (!scanner.hasNextLine()) {
+                throw new WrongLinksFormatException("Файл пустой");
+            }
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] strings = line.split("\t");
+                if (strings.length != 3) {
+                    throw new WrongLinksFormatException("Не все строки в config файле состоят из 3х частей");
+                }
+                Suggest suggest = new Suggest(strings[0], strings[1], strings[2]);
+                allSuggests.add(suggest);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Не могу прочитать файл : " + e);
+        }
+        return allSuggests;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,76 @@
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.annot.PdfLinkAnnotation;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Link;
+import com.itextpdf.layout.element.Paragraph;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // создаём конфиг
+
+    public static void main(String[] args) throws IOException {
+
         LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
+        var pathIn = new File("data/pdfs/");
+        var pathOut = new File("data/converted/");
+        convertAllPdf(linksSuggester, pathIn, pathOut);
 
-        // перебираем пдфки в data/pdfs
+    }
+    static void convertAllPdf(LinksSuggester linksSuggester, File pathIn, File pathOut) throws IOException {
+        List<Suggest> allSuggests = linksSuggester.getAllSuggests();
+        for (File fileIn : (Objects.requireNonNull(pathIn.listFiles()))) {
+            Map<String, Boolean> existSuggestInPdf = new HashMap<>();
+            for (Suggest sug : allSuggests) {
+                existSuggestInPdf.put(sug.getKeyWord(), false);
+            }
+            File fileOut = new File(pathOut, fileIn.getName());
+            var oldDoc = new PdfDocument(new PdfReader(fileIn));
+            var newDoc = new PdfDocument(new PdfWriter(fileOut));
+            int countOfPages = oldDoc.getNumberOfPages();
+            for (int i = 1; i <= countOfPages; i++) {
+                oldDoc.copyPagesTo(i, i, newDoc);
+                PdfPage page = oldDoc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                List<Suggest> findSuggests = linksSuggester.suggest(text);
+                List<Suggest> pageSuggests = new ArrayList<>();
+                boolean isFind = false;
+                for (Suggest sug : findSuggests) {
+                    if (!existSuggestInPdf.get(sug.getKeyWord())) {
+                        pageSuggests.add(sug);
+                        existSuggestInPdf.put(sug.getKeyWord(), true);
+                        isFind = true;
+                    }
+                }
+                if (isFind) {
+                    var newPage = newDoc.addNewPage();
+                    var rect = new Rectangle(newPage.getPageSize()).moveRight(10).moveDown(10);
+                    Canvas canvas = new Canvas(newPage, rect);
+                    Paragraph paragraph = new Paragraph("Suggestions:\n");
+                    paragraph.setFontSize(25);
 
-        // для каждой пдфки создаём новую в data/converted
+                    for (Suggest suggest : pageSuggests) {
+                        PdfLinkAnnotation annotation = new PdfLinkAnnotation(rect);
+                        PdfAction action = PdfAction.createURI(suggest.getUrl());
+                        annotation.setAction(action);
+                        Link link = new Link(suggest.getTitle(), annotation);
+                        paragraph.add(link.setUnderline());
+                        paragraph.add("\n");
+                    }
 
-        // перебираем страницы pdf
-
-        // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
-
-        // вставляем туда рекомендуемые ссылки из конфига
+                    canvas.add(paragraph);
+                }
+            }
+            oldDoc.close();
+            newDoc.close();
+        }
     }
 }
